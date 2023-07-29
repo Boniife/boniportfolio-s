78946647a1217b9d24f43879bc392ca2f1312966
boniife <131681425+Boniife@users.noreply.github.com>
2023-07-29 16:14:02 +0100

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 const Header = () => {
+  const [Toggle, showMenu] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav_logo">
           Ifeanyi
         </a>
-        <div className="nav_menu">
+        <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
           <ul className="nav_list grid">
             <li className="nav_item">
               <a href="#home" className="nav_link active-link">
@@ -42,7 +43,7 @@ const Header = () => {
           </ul>
           <i class="uil uil-times nav_close"></i>
         </div>
-        <div className="nav_toggle">
+        <div className="nav_toggle" onClick={() => showMenu(!Toggle)}>
           <i class="uil uil-apps"></i>
         </div>
       </nav>
